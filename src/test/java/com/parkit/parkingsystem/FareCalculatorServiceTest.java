d1224fbfe941d5a7b7ff4d680c28166f0c28b206
joselct17 <62754339+joselct17@users.noreply.github.com>
2022-12-07 11:23:10 +0100

    rajout du test pour 30minutes

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -125,11 +125,21 @@ public class FareCalculatorServiceTest {
         assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
     }
 
-    @Ignore("not yet ready")
-    @Test
-    public void testACoder() {
 
-        fail("a coder");
+    @Test
+    public void calculateFareCarWithLessThanHalfAnHourParkingTime() {
+
+        Date inTime = new Date();
+        inTime.setTime(System.currentTimeMillis() - (30 * 60 * 1000)); // 30 minutes parking gratuit
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals((0 * Fare.CAR_RATE_PER_HOUR), ticket.getPrice());
+
     }
 
 }
